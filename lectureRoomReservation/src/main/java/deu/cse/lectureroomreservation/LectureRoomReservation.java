2567de5c687d8095f9efde82989a43b475c5307b
minyook <87015931+minyook@users.noreply.github.com>
2025-05-08 13:11:37 +0900

    test
    test

---
+++ lectureRoomReservation/src/main/java/deu/cse/lectureroomreservation/LectureRoomReservation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package deu.cse.lectureroomreservation;
+
+/**
+ *
+ * @author limmi
+ */
+public class LectureRoomReservation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
